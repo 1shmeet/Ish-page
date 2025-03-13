c3d0435e66eceac1df52ec26442e3af6c0da886f
ISHMEET SINGH <92634396+1shmeet@users.noreply.github.com>
2025-03-13 11:27:06 +0530

    Update app.js

--- backend/app.js
+++ backend/app.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors({
-  origin: 'https://your-frontend-url.com',  // Change this to your frontend URL
+  origin: 'https://singh-1shmeet.web.app',  // Change this to your frontend URL
   methods: ['GET', 'POST'],
   allowedHeaders: ['Content-Type']
 }));
